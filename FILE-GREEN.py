474e59cbfcc4aadd77cce5e9023d6b10b4f7dbe6
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-09-13 15:19:49 +0600

    Update FILE-GREEN.py

--- FILE-GREEN.py
+++ FILE-GREEN.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from MAHADIF import Subscraption
+        from FILE import Subscraption
  
         Subscraption()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from OK import Subscraption
+        from FILE32 import Subscraption
  
  
         Subscraption()
